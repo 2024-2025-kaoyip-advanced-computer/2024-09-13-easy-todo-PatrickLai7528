0437dee3e4e71fccdccc0b379ca3b77b76ec4937
Patrick Lai <laikinemngpatrick@gmail.com>
2024-10-24 00:51:06 +0000

    1024

--- my-app/src/Component.js
+++ my-app/src/Component.js
@@ -1,14 +1,24 @@
 export const SuperLaiSir = () => {
+    const schoolNames = ["教業", "培道", '培正', '鏡平', '勞校']
     return (
         <>
             <h1>超級賴SIR</h1>
-            <SuperSchool schoolName="教業"/>
+            {
+                schoolNames.map(function (schoolName) {
+                    return schoolName === "教業" ? null : <SuperSchool schoolName={schoolName} />
+                })
+            }
         </>
     )
 }
 
 function SuperSchool(props) {
-    return <h2>超級  {props.schoolName === "教業" ? "超級":""}   {props.schoolName}</h2>
+    return <>
+        <h2>超級  {props.schoolName === "教業" ? "超級" : ""}   {props.schoolName}</h2>
+        {
+            props.schoolName === "教業" ? <span>我愛教業</span> : null
+        }
+    </>
     /**
      * 如果schoolName是教業,  用span標籤, 顯示我愛教業
      */
