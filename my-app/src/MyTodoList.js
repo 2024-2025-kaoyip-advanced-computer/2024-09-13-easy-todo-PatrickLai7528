627d636ab83c01eb6b4b1064088f51af01f394f9
Patrick Lai <laikinemngpatrick@gmail.com>
2025-04-10 01:55:46 +0000

    20250410

--- my-app/src/MyTodoList.js
+++ my-app/src/MyTodoList.js
@@ -3,85 +3,30 @@ import { useState, useEffect } from 'react'
 import { supabase } from './utils/supabase'
 const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
       render: (text) => <a>{text}</a>,
     },
     {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-    {
-      title: 'Tags',
-      key: 'tags',
-      dataIndex: 'tags',
-      render: (_, { tags }) => (
-        <>
-          {tags.map((tag) => {
-            let color = tag.length > 5 ? 'geekblue' : 'green';
-            if (tag === 'loser') {
-              color = 'volcano';
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </>
-      ),
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (_, record) => (
-        <Space size="middle">
-          <a>Invite {record.name}</a>
-          <a>Delete</a>
-        </Space>
-      ),
-    },
-  ];
-  const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      tags: ['nice', 'developer'],
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      tags: ['loser'],
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
-      tags: ['cool', 'teacher'],
-    },
+      title:"標題",
+      dataIndex: 'title',
+      key:"title",
+    }
   ];
 
 export const MyTodoList = () => {
+  const [todos, setTodos] = useState([])
   useEffect(() => {
+    // 異步
     async function getTodos() {
       const { data } = await supabase.from('todo').select()
+      setTodos(data)
       console.log(data)
     }
 
     getTodos()
   }, [])
 
-    return <Table columns={columns} dataSource={data} />
+    return <Table columns={columns} dataSource={todos} />
 }
