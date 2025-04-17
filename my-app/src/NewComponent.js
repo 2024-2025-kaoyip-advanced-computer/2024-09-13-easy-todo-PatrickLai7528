6181d2f999c92a8d6fbd9b21da3b65c5a6771611
Patrick Lai <laikinemngpatrick@gmail.com>
2025-04-17 00:53:09 +0000

    20250417

--- my-app/src/NewComponent.js
+++ my-app/src/NewComponent.js
@@ -1,7 +1,8 @@
-import { Layout } from 'antd'; // 模塊引入
-import {MyMenu} from "./MyMenu"
+import React, { useState } from 'react';
+import { Layout, Button, Modal, Input } from 'antd'; // 模塊引入
+import { MyMenu } from "./MyMenu"
 
-import {MyTodoList} from "./MyTodoList"
+import { MyTodoList } from "./MyTodoList"
 
 //const { Header, Footer, Sider, Content } = Layout; // 對象解構
 
@@ -10,14 +11,34 @@ const Sider = Layout.Sider;
 const Content = Layout.Content;
 
 export const MyLayout = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleOk = () => {
+      setIsModalOpen(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };
+
+
     return <Layout style={{ height: "100vh" }} >
         <Header >Header</Header>
         <Layout>
             <Sider width={255} >
-                <MyMenu/>
+                <MyMenu />
             </Sider>
             <Content>
-                <MyTodoList/>
+                <Button onClick={showModal}>增加</Button>
+                <MyTodoList />
+                <Modal title="新建待辦事項" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                    <Input placeholder="請輸入標題" />
+                    <Input.TextArea rows={4} placeholder='請輸入描述'/>
+                </Modal>
             </Content>
         </Layout>
     </Layout>
